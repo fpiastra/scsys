b2593b9a080aa9146ab14ede4340c3f2d056d1aa
Francesco Piastra <f.piastra@gmail.com>
2026-09-26 17:50:45 +0200

    Minor renaming in the notifiers submodule

--- scsys/alarms/notifiers/__init__.py
+++ scsys/alarms/notifiers/__init__.py
@@ -1,9 +1,9 @@
-NOTIFIERS: dict[str, AlarmNotifier] = {}
+NOTIFIER_CLASSES: dict[str, AlarmNotifier] = {}
 
 def discover_notifiers():
     pass
 #
 
 def get_notifier(name):
-    return NOTIFIERS.get(name)
+    return NOTIFIER_CLASSES.get(name)
 #
